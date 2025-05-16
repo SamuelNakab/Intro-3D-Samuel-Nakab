28403b3d89188d843daca1aa7c8ecc8e15bc1551
SamuelNakab <167198088+SamuelNakab@users.noreply.github.com>
2025-05-16 10:37:58 -0300

    Terminado clonaCubos

---
+++ Assets/ClonaCubos.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class ClonaCubos : MonoBehaviour
+{
+    public GameObject cuboPrefab;
+
+    void Start()
+    {
+        
+    }
+    public void ClonarCubos()
+    {
+        Instantiate(cuboPrefab);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+}
